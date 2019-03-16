6dd9b5bd2db8e2aa12b017ce8c7695948b578574
Ebenezer <ebenezerdonu@gmail.com>
2019-03-17 00:13:28 +0100

    [feature: #164678746] add migration file for user

---
+++ migrations/001-create-user.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    username: {
+      type: Sequelize.STRING,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    isAdmin: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
+};
